d28672957bb2475276f8b0ee35c575e584c22580
nkalpana <nkalpana@gmail.com>
2019-01-06 04:28:56 -0500

    Convert project to typescript, and basic layout`

---
+++ src/components/Student.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+export default class Student extends React.Component {
+  public render() {
+    return <div><p>Students -  List students, and Add student </p></div>;
+  }
+}
